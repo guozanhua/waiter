95fa9b32892bda5fb13ad4de3380178f3b77d73a
sauerbraten <willing.alexander@googlemail.com>
2014-01-15 21:55:41 +0100

    moved Packet type + its methods into own file; changed pop* methods to get* methods to leave the original packet unchanged

---
+++ packet.go
@@ -0,0 +1,81 @@
+package main
+
+type Packet struct {
+	buf []byte
+	pos int
+}
+
+// Encodes an int32 and appends it to the packet.
+func (p *Packet) putInt32(i int32) {
+	if i < 128 && i > -127 {
+		p.buf = append(p.buf, byte(i))
+	} else if i < 0x8000 && i >= -0x8000 {
+		p.buf = append(p.buf, 0x80, byte(i), byte(i>>8))
+	} else {
+		p.buf = append(p.buf, 0x81, byte(i), byte(i>>8), byte(i>>16), byte(i>>24))
+	}
+}
+
+// Appends a string to the packet.
+func (p *Packet) putString(s string) {
+	for _, c := range s {
+		p.putInt32(int32(c))
+	}
+	p.putInt32(0)
+}
+
+// Advances the position in the packet by n bytes, returns an error if the end of the packet is reached or crossed.
+func (p *Packet) step(n int) error {
+	pos += n
+	if pos >= len(p.buf) {
+		return errors.New("end of packet")
+	}
+}
+
+// Returns the first byte in the Packet.
+func (p *Packet) getByte() (byte, error) {
+	return p.buf[pos], p.step(1)
+}
+
+// Decodes an int32 and increases the position index accordingly.
+func (p *Packet) getInt32() int32 {
+	i := int32(p.getByte())
+
+	if i == 0x80 {
+		return int32(p.getByte()) + (int32(p.getByte()) << 8)
+	} else if i == 0x81 {
+		return int32(p.getByte()) + (int32(p.getByte()) << 8) + (int32(p.getByte()) << 16) + (int32(p.getByte()) << 24)
+	} else {
+		return i
+	}
+}
+
+// Decodes an int32 using the different compression meant for uint32s and increases the position index accordingly.
+func (p *Packet) getUint32() int32 {
+	i := int32(p.getByte())
+	if i >= 0x80 {
+		i += int32(p.getByte()<<7) - 0x80
+		if i >= (1 << 14) {
+			i += int32(p.getByte()<<14) - (1 << 14)
+		}
+		if i >= (1 << 21) {
+			i += int32(p.getByte()<<21) - (1 << 21)
+		}
+		if i >= (1 << 28) {
+			i |= -(1 << 28)
+		}
+	}
+
+	return i
+}
+
+// Reads a string from the packet and increases the position index accordingly.
+func (p *Packet) popString() string {
+	buf := []byte{}
+
+	for b := p.getByte(); b != 0x00; b = p.getByte() {
+		buf = append(buf, b)
+	}
+
+	return string(buf)
+}
